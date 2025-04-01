0c3ccf7bda4c47a000fb80e7a3a3951f69aee8e1
Sophia Vadstrup <102520250+Svadst@users.noreply.github.com>
2025-04-01 17:24:35 +0200

    Albumin

---
+++ Preprocessing/FeatureExtraction/Albumin.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import os
+
+# Load clinical dataset
+clinical_data_url = "https://api.vitaldb.net/cases"
+df_clinical = pd.read_csv(clinical_data_url)
+
+# Extract preop_alb
+df_final = df_clinical[['caseid', 'preop_alb']]
+
+# Save to CSV
+save_path = os.path.join('Preprocessing', 'Data', 'Data_preopAlbumin.csv')
+os.makedirs(os.path.dirname(save_path), exist_ok=True)
+df_final.to_csv(save_path, index=False)
+
+print(f"CSV file 'Data_preop_alb.csv' has been saved successfully at {save_path}.")
