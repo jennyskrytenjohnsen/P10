913573fd944add99dd9916cf87dda3443c4e4ae8
Maria059v <102419240+Maria059v@users.noreply.github.com>
2025-04-07 22:05:58 +0200

    Pulse pressure

--- Preprocessing/FeatureExtraction/AveragePulsePressure.py
+++ Preprocessing/FeatureExtraction/AveragePulsePressure.py
@@ -6,10 +6,11 @@ sysbp_df = pd.read_csv("Preprocessing/Data/Data_AvgSysBP.csv")
 diabp_df = pd.read_csv("Preprocessing/Data/Data_AvgDiaBP.csv")
 
 # Merge on 'caseid'
-merged_df = pd.merge(sysbp_df, diabp_df, on='caseid', how='outer', suffixes=('_SysBP', '_DiaBP'))
-
+merged_df = pd.read_csv("test_mergedPP.csv")
+# merged_df = pd.merge(sysbp_df, diabp_df, on='caseid', how='outer', suffixes=('AvgSysBP', 'AvgDiaBP'))
+# merged_df.to_csv('test_merged.csv', index=False)
 # Calculate Pulse Pressure (PP) as SysBP - DiaBP
-merged_df['PP'] = merged_df['SysBP'] - merged_df['DiaBP']
+merged_df['PP'] = merged_df['AvgSysBP'] - merged_df['AvgDiaBP']
 
 # Keep only caseid and PP
 result_df = merged_df[['caseid', 'PP']]
