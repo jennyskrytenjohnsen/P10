a5bb927e7bc5ac04dfdf57683b337388417a9747
Maria059v <102419240+Maria059v@users.noreply.github.com>
2025-04-17 15:04:02 +0200

    Updated GUI

--- StreamlitGUI/Pages/2_View_variables.py
+++ StreamlitGUI/Pages/2_View_variables.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 
@@ -9,80 +8,127 @@ st.set_page_config(page_title="Variables", page_icon="📊")
 st.markdown("# Variables Affecting the Prediction")
 st.write("This page offers an overview of the variables affecting the prediction of ICU admission. The importance of each variable is determined by the underlying machine learning algorithm, and therefore it might not match the physiological importance.")
 
-# Define 5 columns and 3 rows
-columns = ["Respiratory", "Circulatory", "Renal", "Demographic", "Others"]
-rows = ["Preoperative", "", "Perioperative"]
+# Add color bar legend
+st.markdown("**Importance Scale**")
+fig, ax = plt.subplots(figsize=(8, 0.05))
+cmap = sns.color_palette("coolwarm", as_cmap=True)
+norm = plt.Normalize(0, 100)
+cb1 = plt.colorbar(plt.cm.ScalarMappable(norm=norm, cmap=cmap), cax=ax, orientation='horizontal')
+st.pyplot(fig)
 
-# Each category has one variable per row → 3 variables per category
-# data_dict = {
-#     "Respiratory": [("PaO2", 85), ("x", 50), ("FiO2", 75)],
-#     "Circulatory": [("MAP", 70), ("x", 50), ("HR", 65)],
-#     "Renal": [("Creatinine", 80), ("x", 50), ("BUN", 60)],
-#     "Demographic": [("Age", 50), ("x", 50), ("Sex", 30)],
-#     "Others": [("Comorbidities", 70), ("x", 50), ("Surgical type", 85)]
-# }
+# ----------------- DEMOGRAPHIC VARIABLES -----------------
+st.markdown("#### Demographic Variables")
 
-data_dict = {
-    "Respiratory": [("PaO2", 85), ("x", 50), ("FiO2", 75)],
-    "Circulatory": [("MAP", 70), ("x", 50), ("HR", 65)],
-    "Renal": [("Creatinine", 80), ("x", 50), ("BUN", 60)],
-    "Demographic": [("Age", 50), ("x", 50), ("Sex", 30)],
-    "Others": [("Comorbidities", 70), ("x", 50), ("Surgical type", 85)]
+# Demographic data with importance values
+demo_vars = ["age", "sex", "height", "weight", "BMI"]
+demo_importance = [80, 65, 50, 70, 90]
+
+df_demo = pd.DataFrame({"Demographic": demo_vars})
+importance_demo = pd.Series(demo_importance, index=df_demo.index)
+
+def colorize_demo(val):
+    idx = df_demo[df_demo["Demographic"] == val].index[0]
+    score = importance_demo[idx] / 100
+    color = sns.color_palette("coolwarm", as_cmap=True)(score)
+    return f'background-color: rgba({int(color[0]*255)}, {int(color[1]*255)}, {int(color[2]*255)}, 0.7)'
+
+styled_demo = df_demo.style.applymap(colorize_demo).hide(axis="index")
+st.write(styled_demo)
+
+# ----------------- PREOPERATIVE VARIABLES -----------------
+st.markdown("#### Preoperative Variables")
+
+data_dict_var_pre = {
+    "Respiratory": ["PaO2", "Hej1", "Bla1"],
+    "Circulatory": ["MAP", "Hej2", "Bla2"],
+    "Renal": ["CR", "Hej3", "Bla3"]
 }
-
-data_dict_numbers = {
-    "Respiratory": [(85), (50), (75)],
-    "Circulatory": [(70), (50), (65)],
-    "Renal": [(80), (50), (60)],
-    "Demographic": [(50), (50), (30)],
-    "Others": [(70), (50), (85)]
+data_dict_numbers_pre = {
+    "Respiratory": [85, 60, 45],
+    "Circulatory": [70, 55, 50],
+    "Renal": [75, 40, 30]
 }
+data_pre = pd.DataFrame.from_dict(data_dict_var_pre)
+values_df_pre = pd.DataFrame.from_dict(data_dict_numbers_pre)
+data_pre.reset_index(drop=True, inplace=True)
+values_df_pre.reset_index(drop=True, inplace=True)
 
-data_dict_var = {
-    "Respiratory": [("PaO2"), ("x"), ("FiO2")],
-    "Circulatory": [("MAP"), ("x"), ("HR")],
-    "Renal": [("Creatinine"), ("x"), ("BUN")],
-    "Demographic": [("Age"), ("x"), ("Sex")],
-    "Others": [("Comorbidities"), ("x"), ("Surgical type")]
-}
-
-# Build variable names and values
-variables = [v[0] for cat in columns for v in data_dict[cat]]
-values = [v[1] for cat in columns for v in data_dict[cat]]
-
-# Reshape into 3x5 table
-data = pd.DataFrame.from_dict(data_dict_var)
-data.rename(index={0: "Preoperative", 1: "x", 2: "Perioperative"})
-# data = pd.DataFrame(pd.DataFrame.from_dict(data_dict_var), index=rows, columns=columns)
-print(data)
-# data = pd.DataFrame(np.array(variables).reshape(3, 5), index=rows, columns=columns)
-# values_df = pd.DataFrame(np.array(values).reshape(3, 5), index=rows, columns=columns)
-values_df =pd.DataFrame.from_dict(data_dict_numbers)
-values_df.rename(index={0: "Preoperative", 1: "x", 2: "Perioperative"})
-data.rename(index={0: "Preoperative", 1: "x", 2: "Perioperative"}, inplace=True)
-values_df.rename(index={0: "Preoperative", 1: "x", 2: "Perioperative"}, inplace=True)
-print(data)
-print("----------------")
-print(values_df)
-# Function to apply color based on value
-def colorize(val):
-    for row in data.index:
-        for col in data.columns:
-            if data.loc[row, col] == val:
-                score = values_df.loc[row, col] / 100
+def colorize_pre(val):
+    for i in range(data_pre.shape[0]):
+        for col in data_pre.columns:
+            if data_pre.loc[i, col] == val:
+                score = values_df_pre.loc[i, col] / 100
                 color = sns.color_palette("coolwarm", as_cmap=True)(score)
                 return f'background-color: rgba({int(color[0]*255)}, {int(color[1]*255)}, {int(color[2]*255)}, 0.7)'
     return ''
 
-# Style the table
-styled_table = data.style.applymap(colorize)
-st.write(styled_table)
+styled_table_pre = data_pre.style.applymap(colorize_pre).hide(axis="index")
+st.write(styled_table_pre)
 
-# Add a color bar legend
-st.markdown("Importance Scale")
-fig, ax = plt.subplots(figsize=(8, 0.1))
-cmap = sns.color_palette("coolwarm", as_cmap=True)
-norm = plt.Normalize(0, 100)
-cb1 = plt.colorbar(plt.cm.ScalarMappable(norm=norm, cmap=cmap), cax=ax, orientation='horizontal')
-# cb1.set_label('Importance Score')
-st.pyplot(fig)
+# ----------------- PERIOPERATIVE VARIABLES -----------------
+st.markdown("#### Perioperative Variables")
+
+data_dict_var_peri = {
+    "Respiratory": ["uhu", "skjd", "ski"],
+    "Circulatory": ["MAP", "Hej2", "laj"],
+    "Renal": ["CR", "Hej3", "Bla3"]
+}
+data_dict_numbers_peri = {
+    "Respiratory": [85, 60, 45],
+    "Circulatory": [70, 55, 50],
+    "Renal": [75, 40, 30]
+}
+data_peri = pd.DataFrame.from_dict(data_dict_var_peri)
+values_df_peri = pd.DataFrame.from_dict(data_dict_numbers_peri)
+data_peri.reset_index(drop=True, inplace=True)
+values_df_peri.reset_index(drop=True, inplace=True)
+
+def colorize_peri(val):
+    for i in range(data_peri.shape[0]):
+        for col in data_peri.columns:
+            if data_peri.loc[i, col] == val:
+                score = values_df_peri.loc[i, col] / 100
+                color = sns.color_palette("coolwarm", as_cmap=True)(score)
+                return f'background-color: rgba({int(color[0]*255)}, {int(color[1]*255)}, {int(color[2]*255)}, 0.7)'
+    return ''
+
+styled_table_peri = data_peri.style.applymap(colorize_peri).hide(axis="index")
+st.write(styled_table_peri)
+
+# ----------------- OTHER VARIABLES -----------------
+st.markdown("#### Other Variables")
+
+# Define the variable names and importance scores in transposed format
+data_dict_var_others = {
+    "Surgery details": ["Surgery duration", "Ane duration", "number3"],
+    "Laboratory values": ["hej", "hy", "ju"],
+    "Others": ["", "", ""]
+}
+
+data_dict_importance_others = {
+    "Surgery details": [60, 65, 50],
+    "Laboratory values": [70, 75, 80],
+    "Others": [0, 0, 0]
+}
+
+# Create DataFrames
+df_others = pd.DataFrame.from_dict(data_dict_var_others)
+importance_df_others = pd.DataFrame.from_dict(data_dict_importance_others)
+
+# Reset index to align rows properly
+df_others.reset_index(drop=True, inplace=True)
+importance_df_others.reset_index(drop=True, inplace=True)
+
+# Coloring function for each value based on importance
+def colorize_others(val):
+    for i in range(df_others.shape[0]):
+        for col in df_others.columns:
+            if df_others.loc[i, col] == val:
+                score = importance_df_others.loc[i, col] / 100
+                color = sns.color_palette("coolwarm", as_cmap=True)(score)
+                return f'background-color: rgba({int(color[0]*255)}, {int(color[1]*255)}, {int(color[2]*255)}, 0.7)'
+    return ''
+
+# Apply the styling
+styled_others = df_others.style.applymap(colorize_others).hide(axis="index")
+st.write(styled_others)
