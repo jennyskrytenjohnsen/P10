efef6e5b58d9fbfb2d026508be2e01e498a44a0a
Sophia Vadstrup <102520250+Svadst@users.noreply.github.com>
2025-04-18 10:45:18 +0200

    Updated LogLoss plot for XGBoost

--- Machine/XGBoost_ICU_logLoss_plot.py
+++ Machine/XGBoost_ICU_logLoss_plot.py
@@ -3,15 +3,10 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
-from sklearn.pipeline import Pipeline
 from xgboost import XGBClassifier
-from sklearn.metrics import (
-    accuracy_score, precision_score, recall_score,
-    f1_score, roc_auc_score, roc_curve, confusion_matrix
-)
 
 # Load csv file with features 
-df_data_features = pd.read_csv("df_so_far_extracted_features.csv")
+df_data_features = pd.read_csv("df_extracted_features.csv")
 # print(df_data_features.head())
 
 # Load csv file with labels
@@ -46,7 +41,10 @@ X_test = df_data_features[test_mask].drop(columns=["caseid"])
 y_train = y[train_mask]
 y_test = y[test_mask]
 
-# Build pipeline with XGBoost (no imputer is included) 
+# Define eval set, creating a list of dataseta to evaluate during training   
+eval_set = [(X_train, y_train), (X_test, y_test)]
+
+# Build XGBoost
 # the pipeline is not used when plotting the logloss
 # With tuned hyperparameters:
 # - learning rate and n_estimators can improve generalization
@@ -56,7 +54,7 @@ y_test = y[test_mask]
 # - eval_metric='logloss' is a common choice for binary classification
 # Build model separately (no pipeline) so we can access evals_result_
 model = XGBClassifier(
-    n_estimators=120,
+    n_estimators=200,
     max_depth=4,
     learning_rate=0.05,
     subsample=0.8,
@@ -68,42 +66,20 @@ model = XGBClassifier(
 )
 
 # Train the model
-model.fit(X_train, y_train)
-# Predict
-y_pred = model.predict(X_test)
-y_prob = model.predict_proba(X_test)[:, 1] 
-
-
-# Define eval set
-eval_set = [(X_train, y_train), (X_test, y_test)]
-
-# Train the model with evaluation set
 model.fit(
     X_train,
     y_train,
     eval_set=eval_set,
-    verbose=False  # You can set True if you want to see live printing
+    verbose=False
 )
+
+# Predict
+y_pred = model.predict(X_test)
+y_prob = model.predict_proba(X_test)[:, 1] 
 
 # Access the logloss evolution
 results = model.evals_result()
 
-#check stratification in test and train 
-print("Train set ICU label distribution:")
-print(y_train.value_counts(normalize=True))
-
-print("\nTest set ICU label distribution:")
-print(y_test.value_counts(normalize=True))
-
-print("\n Performance Metrics:")
-print(f"Accuracy:  {accuracy_score(y_test, y_pred):.3f}")
-print(f"Precision: {precision_score(y_test, y_pred):.3f}")
-print(f"Recall:    {recall_score(y_test, y_pred):.3f}")
-print(f"F1 Score:  {f1_score(y_test, y_pred):.3f}")
-print(f"ROC AUC:   {roc_auc_score(y_test, y_prob):.3f}")
-
-print("\n Confusion Matrix:")
-print(confusion_matrix(y_test, y_pred))
 
 # Plot logloss
 plt.figure()
