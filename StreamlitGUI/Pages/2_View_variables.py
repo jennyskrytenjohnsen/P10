a8673406a353833be4e75e6b59ac84f239139f2b
Maria059v <102419240+Maria059v@users.noreply.github.com>
2025-05-07 14:17:09 +0200

    Working GUI 2 columns

--- StreamlitGUI/Pages/2_View_variables.py
+++ StreamlitGUI/Pages/2_View_variables.py
@@ -5,37 +5,27 @@ import matplotlib.pyplot as plt
 
 st.set_page_config(page_title="Variables", page_icon="📊")
 
+# Top part spanning the full width
+st.markdown("# Variables Affecting the Prediction")
+    
+# Display the selected patient's name
+st.markdown(f"### Selected: {st.session_state.patient_option}")
+
+st.write("This page offers an overview of the variables affecting the prediction of ICU admission. The importance of each variable is determined by the underlying machine learning algorithm, and therefore it might not match the physiological importance.")
+
+# Add color bar legend
+st.markdown("**Importance Scale**")
+fig, ax = plt.subplots(figsize=(8, 0.05))
+cmap = sns.color_palette("coolwarm", as_cmap=True)
+norm = plt.Normalize(0, 100)
+cb1 = plt.colorbar(plt.cm.ScalarMappable(norm=norm, cmap=cmap), cax=ax, orientation='horizontal')
+st.pyplot(fig)
 
 # Create two columns: left for content, right for patient name
-col1, col2 = st.columns([15, 1])  # Adjust width ratio as needed
-
-# Display only patient name in the right column
-# with col2:
-#     if "patient_option" in st.session_state:
-#         st.markdown(
-#             f"""
-#             Selected Patient: {st.session_state.patient_option}
-#             """,
-#             unsafe_allow_html=True
-#         )
+col1, col2 = st.columns([1, 1])  # Adjust width ratio as needed
 
 # Put all other content inside col1
 with col1:
-    st.markdown("# Variables Affecting the Prediction")
-    
-    # Display the selected patient's name
-    st.markdown(f"### Selected: {st.session_state.patient_option}")
-
-    st.write("This page offers an overview of the variables affecting the prediction of ICU admission. The importance of each variable is determined by the underlying machine learning algorithm, and therefore it might not match the physiological importance.")
-
-    # Add color bar legend
-    st.markdown("**Importance Scale**")
-    fig, ax = plt.subplots(figsize=(8, 0.05))
-    cmap = sns.color_palette("coolwarm", as_cmap=True)
-    norm = plt.Normalize(0, 100)
-    cb1 = plt.colorbar(plt.cm.ScalarMappable(norm=norm, cmap=cmap), cax=ax, orientation='horizontal')
-    st.pyplot(fig)
-
     # ----------------- DEMOGRAPHIC VARIABLES -----------------
     st.markdown("#### Demographic Variables")
     demo_vars = ["age", "sex", "height", "weight", "BMI"]
@@ -52,46 +42,16 @@ with col1:
     styled_demo = df_demo.style.applymap(colorize_demo).hide(axis="index")
     st.write(styled_demo)
 
-    # ----------------- PREOPERATIVE VARIABLES -----------------
-    st.markdown("#### Preoperative Variables")
-    data_dict_var_pre = {
-        "Respiratory": ["PaO2", "Hej1", "Bla1"],
-        "Circulatory": ["MAP", "Hej2", "Bla2"],
-        "Renal": ["CR", "Hej3", "Bla3"]
-    }
-    data_dict_numbers_pre = {
-        "Respiratory": [85, 60, 45],
-        "Circulatory": [70, 55, 50],
-        "Renal": [75, 40, 30]
-    }
-    data_pre = pd.DataFrame.from_dict(data_dict_var_pre)
-    values_df_pre = pd.DataFrame.from_dict(data_dict_numbers_pre)
-    data_pre.reset_index(drop=True, inplace=True)
-    values_df_pre.reset_index(drop=True, inplace=True)
-
-    def colorize_pre(val):
-        for i in range(data_pre.shape[0]):
-            for col in data_pre.columns:
-                if data_pre.loc[i, col] == val:
-                    score = values_df_pre.loc[i, col] / 100
-                    color = sns.color_palette("coolwarm", as_cmap=True)(score)
-                    return f'background-color: rgba({int(color[0]*255)}, {int(color[1]*255)}, {int(color[2]*255)}, 0.7)'
-        return ''
-
-    styled_table_pre = data_pre.style.applymap(colorize_pre).hide(axis="index")
-    st.write(styled_table_pre)
-
+    
     # ----------------- PERIOPERATIVE VARIABLES -----------------
     st.markdown("#### Perioperative Variables")
     data_dict_var_peri = {
-        "Respiratory": ["uhu", "skjd", "ski"],
-        "Circulatory": ["MAP", "Hej2", "laj"],
-        "Renal": ["CR", "Hej3", "Bla3"]
+        "Respiratory": ["RR_total", "RR_n12", "RR_n20", "RR_w15minMV", "RR_w15min", "SpO2_total", "SpO2_w15min", "SpO2_n90", "SpO2_w15minMV", "data_vent", "", "", "", "", ""],
+        "Circulatory": ["HR_n30", "HR_n60", "HR_n100", "HR_total", "HR_w15minMV", "value_eph", "value_phe", "value_vaso", "value_ino", "has_aline", "FFP", "RBC", "under36", "over38", "differencebetween15min"],
     }
     data_dict_numbers_peri = {
-        "Respiratory": [85, 60, 45],
-        "Circulatory": [70, 55, 50],
-        "Renal": [75, 40, 30]
+        "Respiratory": [85, 60, 45, 4, 5, 6, 7, 8, 9, 80, 50, 50, 50, 50, 50],
+        "Circulatory": [70, 55, 50, 85, 60, 45, 4, 5, 6, 7, 8, 9, 80, 5, 50],
     }
     data_peri = pd.DataFrame.from_dict(data_dict_var_peri)
     values_df_peri = pd.DataFrame.from_dict(data_dict_numbers_peri)
@@ -110,17 +70,46 @@ with col1:
     styled_table_peri = data_peri.style.applymap(colorize_peri).hide(axis="index")
     st.write(styled_table_peri)
 
+    
+with col2:
+    # ----------------- PREOPERATIVE VARIABLES -----------------
+    st.markdown("#### Preoperative Variables")
+    data_dict_var_pre = {
+        "Circulatory": ["prept", "preaptt", "prehb", "preplt"],
+        "Renal": ["prek", "prena", "preca", ""]
+    }
+    data_dict_numbers_pre = {
+        "Circulatory": [70, 55, 50, 8],
+        "Renal": [75, 40, 30, 50]
+    }
+    data_pre = pd.DataFrame.from_dict(data_dict_var_pre)
+    values_df_pre = pd.DataFrame.from_dict(data_dict_numbers_pre)
+    data_pre.reset_index(drop=True, inplace=True)
+    values_df_pre.reset_index(drop=True, inplace=True)
+
+    def colorize_pre(val):
+        for i in range(data_pre.shape[0]):
+            for col in data_pre.columns:
+                if data_pre.loc[i, col] == val:
+                    score = values_df_pre.loc[i, col] / 100
+                    color = sns.color_palette("coolwarm", as_cmap=True)(score)
+                    return f'background-color: rgba({int(color[0]*255)}, {int(color[1]*255)}, {int(color[2]*255)}, 0.7)'
+        return ''
+
+    styled_table_pre = data_pre.style.applymap(colorize_pre).hide(axis="index")
+    st.write(styled_table_pre)
+
     # ----------------- OTHER VARIABLES -----------------
     st.markdown("#### Other Variables")
     data_dict_var_others = {
-        "Surgery details": ["Surgery duration", "Ane duration", "number3"],
-        "Laboratory values": ["hej", "hy", "ju"],
-        "Others": ["", "", ""]
+        "Others": ["preop_dm", "preop_htn", "asa", "cancer", ""],
+        "Anesthesia": ["generalAnesthesia", "spinalAnesthesia", "sedationalgesia", "anesthesia_duration", "op_duration_min"],
+        "Surgical category": ["General surgery", "Thoracic surgery", "Urology", "Gynecology", ""]
     }
     data_dict_importance_others = {
-        "Surgery details": [60, 65, 50],
-        "Laboratory values": [70, 75, 80],
-        "Others": [0, 0, 0]
+        "Others": [60, 65, 50, 70, 50],
+        "Anesthesia": [70, 75, 80, 10, 20],
+        "Surgical category": [0, 0, 0, 5, 50]
     }
     df_others = pd.DataFrame.from_dict(data_dict_var_others)
     importance_df_others = pd.DataFrame.from_dict(data_dict_importance_others)
