980e69b9068019c21e0d770170699b0a60d17bfe
jennyskrytenjohnsen <jsjo20@student.aau.dk>
2025-05-13 10:40:29 +0200

    :)

--- TestTrainingSet/explore_subjectid.py
+++ TestTrainingSet/explore_subjectid.py
@@ -1,27 +1,29 @@
 import pandas as pd
 
-train = pd.read_csv("C:/Users/johns/Documents/10semester/P10/TestTrainingSet/train_ids_pre&peri.csv")
-test = pd.read_csv("C:/Users/johns/Documents/10semester/P10/TestTrainingSet/test_ids_pre&peri.csv")
+train = pd.read_csv("C:/Users/johns/Documents/10semester/P10/TestTrainingSet/train_ids_pre.csv")
+test = pd.read_csv("C:/Users/johns/Documents/10semester/P10/TestTrainingSet/test_ids_pre.csv")
+
+
+print('Columns in train', len(train.columns))
+print('Rows in train', len(train))
+
 
 clinical_information_url = "https://api.vitaldb.net/cases"
 df_clinical= pd.read_csv(clinical_information_url)
 
-subjectid_caseid = df_clinical[['caseid','subjectid']]
-subjectid = df_clinical['subjectid']
+#subjectid_caseid = df_clinical[['caseid','subjectid']]
+#subjectid = df_clinical['subjectid']
 
-print(subjectid_caseid.head())
+#print(subjectid_caseid.head())
 
-#df_merged = df_data_features.merge(df_data_labels, on="caseid")
+subject_id_train = train['subjectid']
+subjcet_id_test = test['subjectid']
 
-new_df_for_test = train.merge(subjectid_caseid, on="caseid")
+shared = pd.Series(list(set(subject_id_train) & set(subjcet_id_test)))
+print("Felles verdier:", shared.tolist())
 
-print('Len merged', len(new_df_for_test))
-print('Len not merged', len(train))
+#repeated_values = subject_id[subject_id.duplicated(keep=False)]
+#unique_repeated_values = repeated_values.unique()
 
-subidnewdf=new_df_for_test['subjectid']
-
-repeated_values = subidnewdf[subidnewdf.duplicated(keep=False)]
-unique_repeated_values = repeated_values.unique()
-
-print(unique_repeated_values)
+#print(unique_repeated_values)
 
